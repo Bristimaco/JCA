7b6d7330320fda4c3e3918774bc7e4ff05e208d5
Stijn Mattheus <stijn.mattheus@profunctional.be>
2026-03-20 11:16:56 +0100

    Update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,7 +9,9 @@
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
 </head>
+
 <body>
     @inertia
 </body>
+
 </html>
