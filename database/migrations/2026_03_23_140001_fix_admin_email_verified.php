3e5df9b2d2de76529eccf829e8e40f1b247b081d
Stijn Mattheus <stijn.mattheus@profunctional.be>
2026-03-23 14:12:10 +0100

    Update

---
+++ database/migrations/2026_03_23_140001_fix_admin_email_verified.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        User::where('email', 'admin@jca.be')
+            ->whereNull('email_verified_at')
+            ->update(['email_verified_at' => now()]);
+    }
+
+    public function down(): void
+    {
+        //
+    }
+};
