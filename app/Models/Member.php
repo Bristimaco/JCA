16db2ca8201bc36156ece003d54f56c42a740dac
Stijn Mattheus <stijn.mattheus@profunctional.be>
2026-03-25 09:06:30 +0100

    update

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -76,7 +76,7 @@ class Member extends Model
 
     public function fullName(): string
     {
-        return $this->first_name.' '.$this->last_name;
+        return $this->first_name . ' ' . $this->last_name;
     }
 
     public function isActive(): bool
